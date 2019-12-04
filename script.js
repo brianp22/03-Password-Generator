becb7abfba0a007d6ebeb517fdca3e497cab3c63
Brian Prickril <bprickril22@gmail.com>
2019-12-03 22:04:35 -0500

    added javascript to show the slider value aka password length in real time

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var generateBtnEl = document.getElementById("generate-btn");
 var copyBtnEl = document.getElementById("copy-btn");
+var slideEl = document.getElementById("slide");
+var slideValueEl = document.getElementById("slide-value");
+var inputBoxEl = document.getElementById("input-box");
 
 
 function generator() {
-    var passwordLength = document.getElementById("slide").value;
+
+    var passwordLength = slideEl.value - 1;
 
     var characters = "QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm1234567890*&?%#@!";
-
+    
     var password = "";
-
+    
     for (var i = 0; i<= passwordLength; i++) {
         password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
     }
-
-    document.getElementById("input-box").value = password;
-
+    
+    inputBoxEl.value = password;
 }
 
+slideValueEl.textContent = "Password Length: 15";
+
+slideEl.oninput = function() {
+
+        slideValueEl.textContent = "Password Length: " + slideEl.value;
+}
 
 generateBtnEl.addEventListener("click", function(event) {
     event.preventDefault();
